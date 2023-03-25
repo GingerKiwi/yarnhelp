9bf7a9309e47cf66d627bc965f74aaf443194f8d
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-03-25 18:59:34 -0400

    add howmuchyarn-scarfs.js to test new calcs. paste template code and comments from howmuchyarn01.js

---
+++ howmuchyarn-scarfs.js
@@ -0,0 +1,36 @@
+let yardsPerBall = document.getElementById('yards-per-ball')
+let calculateBtn = document.getElementById("btn-calculate")
+
+const yardsPerBallValue = Number.parseInt(yardsPerBall.value);
+
+// =========================//
+// ---      SCARFS ---      //
+// ========================= //
+
+// Knitting Answers
+
+// let babyPulloverAnswer04 = document.getElementById('answer-pullover-baby-04')
+
+
+// Crochet Answers
+
+//let babyCrochetPulloverAnswer04 = document.getElementById('answer-crochet-pullover-baby-04')
+
+// --- 04 WORSTED WEIGHT (yarn thickness) CALCULATIONS --- //
+
+// --- 04 Pullovers --- //
+
+// 04 Knitting 
+
+// const babyPullover04 = Number(Math.ceil(550 * 1.05/yardsPerBallValue))
+
+// 04 Crochet 
+// Crochet takes approximately 25% more yarn than knitting. (* 1.25). An extra 3% has been added to 1.25 to make sure there is enouch yarn.
+
+// const babyCrochetPullover04 = Number(Math.ceil(550 * 1.28 * 1.05/yardsPerBallValue))
+
+// Use the consts above in Calculations to print answer statements to the <p> inside each categories answer.
+
+// babyPulloverAnswer04.innerHTML = `${babyPullover04} balls of this yarn are needed to knit a baby pullover`
+// babyCrochetPulloverAnswer04.innerHTML = `${babyCrochetPullover04} balls of this yarn are needed to crochet a baby pullover`
+
